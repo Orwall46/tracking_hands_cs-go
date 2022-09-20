122e2a838ff53f79cdd8c7d7aca448feb735666f
Orwall46 <Artashes46@yandex.ru>
2022-09-20 22:18:56 +0500

    finish

--- main.py
+++ main.py
@@ -1,22 +1,51 @@
 import keyboard
-import pymem
-import time 
-from pymem import process
+# import pymem
+# import time 
+import autopy
+import cv2
+import mediapipe as mp
 from offsets import *
 
+# Отслеживаем запущенный процесс под названием csgo.exe
+# _process = pymem.Pymem("csgo.exe")
+# _client = pymem.process.module_from_name(_process.process_handle, "client.dll").lpBaseOfDll
 
-_process = pymem.Pymem("csgo.exe")
-_client = pymem.process.module_from_name(_process.process_handle, "client.dll").lpBaseOfDll
+cap = cv2.VideoCapture(0)
 
+width, height = autopy.screen.size()
+
+hands = mp.solutions.hands.Hands(static_image_mode=False,
+                        max_num_hands=2,
+                        min_tracking_confidence=0.5,
+                        min_detection_confidence=0.60)
+
+mpDraw = mp.solutions.drawing_utils
 
 def main():
     while True:
-        try:
-            if keyboard.is_pressed('space'):
-                print("YES")
-        except pymem.exception.MemoryReadError as ex:
-            print(ex)
+        _, img = cap.read()
+        result = hands.process(img)
+        if result.multi_hand_landmarks:
+            for id, lm in enumerate(result.multi_hand_landmarks[0].landmark):
+                h, w, _ = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                cv2.circle(img, (cx, cy), 3, (255, 0, 255))
+                if id == 8:
+                    cv2.circle(img, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
+                    autopy.mouse.move(cx * width / w, cy * height / h)
 
+                    # Передаем стрельбу. Можно настроить на любой другой триггер ...
+                    # attack = _client + dwForceAttack
+                    # _process.write_int(attack, 5)
+                    # time.sleep(0.01)
+                    # _process.write_int(attack, 4)
+
+            mpDraw.draw_landmarks(img, result.multi_hand_landmarks[0], mp.solutions.hands.HAND_CONNECTIONS)
+        rotated = cv2.rotate(img, cv2.ROTATE_180)
+        cv2.imshow('Eldar Jivi, brats', rotated)
+        cv2.waitKey(1)
+        if keyboard.is_pressed('esc'):
+            break
 
 if __name__ == "__main__":
     main()
---
+++ offsets.py
@@ -0,0 +1,8 @@
+dwLocalPlayer = 0xDC14CC
+dwForceAttack = 0x320DE3C
+m_ArmorValue = 0x117CC
+m_bInReload = 0x32B5
+dwEntityList = 0x4DDD91C
+dwGlowObjectManager = 0x53265D0
+m_iGlowIndex = 0x10488
+dwWeaponTable = 0x522F9B4
